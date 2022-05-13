b6766bae78e2f1b8c468358a904bfc68047d14ef
akshansh <chauhanakshansh57@gmail.com>
2022-05-13 10:12:42 +0530

    Updates

---
+++ precache-manifest.a5837f1c5c54c196bce3fef6d38f44d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "685fe54b6e68063faed4",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/css/main.3eb32f0b.chunk.css"
+  },
+  {
+    "revision": "685fe54b6e68063faed4",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/js/main.685fe54b.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "f63eda239e4728cde41c",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/js/2.f63eda23.chunk.js"
+  },
+  {
+    "revision": "5e03f6b0fa0fba1d35f9",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/js/runtime~main.5e03f6b0.js"
+  },
+  {
+    "revision": "93af5237e5aab01fb7de0808935c53b4",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/media/myImg.93af5237.svg"
+  },
+  {
+    "revision": "ab34009e36150a2a0f557bcee80c173a",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/media/menu.ab34009e.svg"
+  },
+  {
+    "revision": "c2309b00df37175a36d41f30bb01d96e",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/static/media/information.c2309b00.svg"
+  },
+  {
+    "revision": "7d339169dda12490297d7add7d30e396",
+    "url": "/AkshanshChauhan/Akshansh-Chauhan-Portfolio/index.html"
+  }
+];
